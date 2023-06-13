9ce26c840b84065db88b973ad118aca2267f18e4
Janvarev Vladislav <janvarev@gmail.com>
2023-06-13 18:33:12 +0300

    examle of row render in video

--- demo_run_video.py
+++ demo_run_video.py
@@ -4,10 +4,13 @@ if __name__ == '__main__':
     chain_processor = ChainVideoProcessor()
     chain_processor.init_with_plugins()
 
-
     chain_processor.run_video_chain("demo_video.mp4","demo_video_res.mp4", fps=25.0, threads=4,
-                                    chain="blur", video_audio="demo_video.mp4") # grayscale affects RGB format, so we don't use it
+                                    chain="blur,to_grayscale", video_audio="demo_video.mp4")
     # video_audio also copy audio stream from original file
     # can work without it
+
+    chain_processor.is_demo_row_render = True
+    chain_processor.run_video_chain("demo_video.mp4", "demo_video_res_row.mp4", fps=25.0, threads=4,
+                                    chain="blur,to_grayscale", video_audio="demo_video.mp4")
 
     print("Video processing completed!")
